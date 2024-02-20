f365026813be368888599e7cfe5f886b4551207d
aaxlss <axl9305@gmail.com>
2024-02-20 23:01:45 +0000

    Adding information to show in the web page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,10 @@ import { Experience, Option } from './Interface';
 import { NgOptimizedImage } from '@angular/common';
 import { ExperienceComponent } from './components/experience/experience.component';
 
+import experience from '../assets/information/experience.json';
+import education from '../assets/information/education.json';
+import certifications from '../assets/information/certifications.json';
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -24,6 +28,7 @@ export class AppComponent {
   menuOptions: Option[] = [];
   hardSkills: Option[] = [];
   experience: Experience[] = [];
+  education: Experience[] = [];
   certifications: Experience[] = [];
   constructor() {
     this.menuOptions = [
@@ -58,32 +63,8 @@ export class AppComponent {
       { label: 'Trello' },
       { label: 'Jira' },
     ];
-    this.experience = [
-      {
-        time: '2022 - PRESENT',
-        title: 'Software Engineer | Upland Alfity',
-        description: `As a Full Stack developer, I specialize in both back-end and front-end development, with a focus on building and enhancing mobile applications for Android and React Native platforms. I am proficient in utilizing Git, GitHub, and Azure as software version controllers, while also implementing CI/CD processes through tools like Jenkins and Azure.
-        I prioritize quality through unit testing, employing JUnit and PHPUnit. Collaborating closely with various departments, including cross-location teams, Quality Control, and fellow developers, I ensure streamlined internal processes and efficient task completion. Additionally, I take pride in my role as a mentor, providing training and guidance to new hires as well as mentoring senior and junior developers in their professional growth.`,
-        skills: [
-          { label: 'SalesForce' },
-          { label: 'Angular' },
-          { label: 'Laravel' },
-          { label: 'Yii2' },
-          { label: 'ECMAScript 6' },
-          { label: 'React Js' },
-          { label: 'React Native' },
-          { label: 'Azure' },
-          { label: 'Postman' },
-          { label: 'Android' },
-        ],
-      },
-    ];
-    this.certifications = [
-      {
-        time: 'OCTOBER 2022',
-        title: 'Microsoft Azure Fundamentals',
-        description: 'nu4a-Dw8E',
-      },
-    ];
+    this.experience = experience;
+    this.education = education;
+    this.certifications = certifications;
   }
 }
